f7ca1319fccb63e324d24511eb9450835005e837
kavoos <kavoos@LAPTOP-K9UTOT8O>
2018-07-21 22:00:53 +0200

    a class has been created

---
+++ demo/src/main/java/com/example/demo/myBean.java
@@ -0,0 +1,18 @@
+package com.example.demo;
+
+public class myBean {
+	
+	private String mystr;
+
+	public String getMystr() {
+		return mystr;
+	}
+
+	public void setMystr(String mystr) {
+		this.mystr = mystr;
+	}
+	
+	
+	
+
+}
